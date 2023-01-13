3068bb4cee7b5c136764e8dd864dbca862e55123
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-01-13 10:13:54 +0700

    menambahkan code pada myCOntroller untuk melakukan get

--- src/main/java/wsb/finalexam/demo/controller/myController.java
+++ src/main/java/wsb/finalexam/demo/controller/myController.java
@@ -4,7 +4,15 @@
  */
 package wsb.finalexam.demo.controller;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import model.entity.Person;
+import model.jpacontroller.PersonJpaController;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,6 +22,33 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @CrossOrigin
+@RequestMapping("/PERSON")
 public class myController {
+    Person person = new Person();
+    PersonJpaController ctrl = new PersonJpaController();
+    
+    @GetMapping("/{id}")
+    public List<Person> getNameById(@PathVariable("id") int id) {
+        List<Person> dummy = new ArrayList<>(); // Declare new LIST
+        try {
+            person = ctrl.findPerson(id); // get data from db
+            dummy.add(person); // fill data from db to list
+        } catch (Exception e) {
+            //dummy = Collections.unmodifiableList(person); // data not found
+        }
+        return dummy;
+    }
+    
+    @GetMapping
+    public List<Person> getAll(){
+        List<Person> dummy = new ArrayList<>(); // Declare new LIST
+        try {
+            dummy = ctrl.findPersonEntities(); // fill data from db to list
+        } catch (Exception e) {
+            //dummy = Collections.unmodifiableList(person); // data not found
+        }
+        return dummy;
+    }
+    
     
 }

3ae392b73baf8847a3d363eaf4f0ecdabe2c1679
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-01-13 09:21:32 +0700

    Initial commite

---
+++ src/test/java/wsb/finalexam/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.finalexam.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

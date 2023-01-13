ebcbdc40f40daed9dc75645669c4847acf216d6c
HabibSantoso <habib.santoso.ft18@mail.umy.ac.id>
2023-01-13 09:53:45 +0700

    menambahkan file myController.java untuk custom controller sendiri

---
+++ src/main/java/wsb/finalexam/demo/controller/myController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package wsb.finalexam.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Habib Santoso
+ * @nim 20180140122
+ */
+@RestController
+@CrossOrigin
+public class myController {
+    
+}
